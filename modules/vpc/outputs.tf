1a8ee5d7d445f6bbb1542b0bb3f2a70156dbb2b7
Manuel Dominguez <manu.i.dominguez@gmail.com>
2025-06-13 12:23:46 -0300

    Added VPC module stored Lambdas in VPC

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,24 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.main.id
+}
+
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
+
+output "lambda_security_group_id" {
+  description = "Security group ID for Lambda functions"
+  value       = aws_security_group.lambda_sg.id
+}
+
+output "s3_vpc_endpoint_id" {
+  description = "S3 VPC Endpoint ID"
+  value       = aws_vpc_endpoint.s3.id
+}
+
+output "dynamodb_vpc_endpoint_id" {
+  description = "DynamoDB VPC Endpoint ID"
+  value       = aws_vpc_endpoint.dynamodb.id
+}
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,29 @@
+variable "name_prefix" {
+  description = "Prefix for resource names"
+  type        = string
+  default     = "recetify"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
